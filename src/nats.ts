2350dec1a0fac2f683a538bd6b8884140e8bdf59
Alberto Ricart <alberto@synadia.com>
2018-08-27 16:05:27 -0500

    version bump, updated doc

--- src/nats.ts
+++ src/nats.ts
@@ -32,7 +32,7 @@ import {ConnectionOptions} from "tls";
 import {next} from 'nuid';
 
 /** Version of the ts-nats library */
-export const VERSION = "1.0.3";
+export const VERSION = "1.0.4";
 
 /**
  * @hidden
